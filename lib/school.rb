247b9449a39f469bae8837381ce26c0716bc2590
Lindsey Trainor <ltrainor38@gmail.com>
2018-01-12 03:36:46 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+class School 
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end 
+  
+  def roster
+    @roster 
+  end 
+
+  def add_student(name, grade)
+  if @roster[grade] == nil 
+    @roster[grade] = []
+  end 
+    @roster[grade] << name 
+  end 
+
+
+def sort 
+ @roster.each_key do |key|
+   @roster[key].sort {|a,b| a[0] <=> b[0]}
+  end 
+   
+  
+end 
+
+def grade(num)
+  @roster[num]
+end 
+
+end 
